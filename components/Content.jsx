e461b51e43a815c9414997108d1e43ca7baea2ca
WonbinLee <wonbin145@gmail.com>
2024-08-14 23:02:50 +0900

    스탠다드반 1차 과제 1번 문제

---
+++ components/Content.jsx
@@ -0,0 +1,5 @@
+const Content = () => {
+  return <p>Welcome to my website!</p>;
+};
+
+export default Content;
---
+++ components/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer>© 2024 My Website</footer>;
+};
+
+export default Footer;
